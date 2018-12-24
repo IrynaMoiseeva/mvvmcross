e0fc0bbee442317f3801fe83a000fe118768dc7c
Irina Moiseeva <irynamoiseeva@gmail.com>
2018-12-24 18:28:53 +0200

    adding files

---
+++ MvvmCross Application1/MvvmCross_Application1.Core/DataBase/DbConnectionManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MvvmCross.Platform;
+using MvvmCross_Application1.Core.Services;
+using MvvmCross_Application1.Core.ViewModels;
+using SQLite;
+
+namespace MvvmCross_Application1.Core.DataBase
+{
+
+    public class DbConnectionManager: IDbConnectionManager
+    {
+        private readonly IPlatformService sqlitePlatformContext;
+        public static SQLiteAsyncConnection connection;
+        private static object syncObj = new Object();
+        public string dbPath;
+        private volatile IOperationsAsync asyncConnection;
+
+        public DbConnectionManager(IPlatformService sqlitePlatformContext)
+        {
+            this.sqlitePlatformContext = sqlitePlatformContext;
+            dbPath = sqlitePlatformContext.DestinationPath;
+        }
+
+         public void DropConnection()
+          {
+              if (asyncConnection == null)
+                  return;
+
+              lock (syncObj)
+              {
+                  asyncConnection = null;
+              }
+          }
+
+          public IOperationsAsync GetConnection()
+          {
+              if (asyncConnection != null)
+                  return asyncConnection;
+
+              lock (syncObj)
+              {
+                  if (asyncConnection != null)
+                      return asyncConnection;
+
+                  var sqLiteAsyncConnection = CreateSqLiteAsyncConnection();
+                  asyncConnection = new SqliteConnectionOperationsAsync(sqLiteAsyncConnection);
+              }
+
+              return asyncConnection;
+          }
+
+
+        private SQLiteAsyncConnection CreateSqLiteAsyncConnection()
+        {
+            var sqLiteAsyncConnection = new SQLiteAsyncConnection(dbPath);
+           
+            return sqLiteAsyncConnection;
+        }
+    }
+}
